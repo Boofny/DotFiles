e6801583d31c270323f16983a28a3324a3613dc9
Boofny <Brandonpizano7557@gmail.com>
2026-01-13 09:22:12 -0500

    made the better version fg (find gitthings)

--- scripts/fuzzy.sh
+++ scripts/fuzzy.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 
-# choice=$(fzf)
-choice=$( fd . ./gitThigns/ --type d --exclude node_modules | fzf )
-# echo "$choice"
+RED='\e[0;31m'
+GREEN='\e[0;32m'
+NC='\e[0m' # No Color Reset
 
-# dir=$(dirname $choice)
-# fullPath=$($choice | pwd)
+y_flag=false
 
-read -p "Make tmux session in ($choice)? [y\n] " option
+while getopts 'y' flag; do # check flag status
+  case "${flag}" in
+    y) y_flag=true;;
+    *) exit 1
+  esac
+done
 
-if [[ $option == "y" || $option == "" ]]; then
- cd $choice && tmux
+if $y_flag; then # if y flag (the yes) flag is used bypass comfirmation option
+  choice=$( fd . ~/gitThings/ --type d --exclude node_modules | fzf )
+  cd $choice && tmux
+else
+  command
+  choice=$( fd . ~/gitThings/ --type d --exclude node_modules | fzf )
+
+  # read -p "${RED}Make tmux session in ($choice)? [y\n] ${NC}" option
+  printf "%b" "Make tmux session in (${GREEN}$choice${NC})? [y/n]"
+  read option
+
+  if [[ $option == "y" || $option == "" ]]; then
+   cd $choice && tmux
+  fi
+
+  if [[ $option == "n" ]]; then
+    echo "Exiting..."
+    exit 1 
+  fi
+
+  # TODO: if no go back to fzf and loop until YES or exit
+  if [[ $option != "n" || $option != "y" || $option == "" ]]; then 
+    echo "Not an option"
+    exit 1
+  fi
 fi
-
-if [[ $option == "n" ]]; then
-  echo "Exiting..."
-  exit 1 
-fi
-
-if [[ $option != "n" || $option != "y" || $option == "" ]]; then
-  echo "Not an option"
-  exit 1
-fi
-
-
-# fd . ./gitThigns/ --type d --exclude node_modules .git | fzf
