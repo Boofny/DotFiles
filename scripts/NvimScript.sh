945afad0f64f3619551b7c9243dd297cad580ede
Boofny <Brandonpizano7557@gmail.com>
2026-01-17 11:14:58 -0500

    made some changes to cdf and made a cool neovim script

--- scripts/NvimScript.sh
+++ scripts/NvimScript.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-echo "wordl"
-
-
 # what this file will kinda be will be like if neovim did not need fzf
 # when the script is ran like nvim and not args then open fzf and exlude things like node_modls and hidden files
 #
 # if nvim is ran with a file name or a dir then open as normal 
 #   if it is a dir then open fzf in that dir 
 
+if [[ "$1" == "" ]]; then  # just the neovim command alone open the current dir
+  choice=$( fd . ./ --type file --exclude node_modules -E go -E .git | fzf --preview 'bat --style=numbers --color=always --line-range :500 {}') || exit 0
+  nvim $choice
+  exit 0
+fi
+
+if [[ -d "$1" ]]; then # this checks if a input is a file or dir
+  choice=$( fd . ./"$1" --type file --exclude node_modules -E go -E .git | fzf --preview 'bat --style=numbers --color=always --line-range :500 {}') || exit 0
+  echo $choice 
+  nvim $choice
+  echo "is a dir"
+  exit 0
+else 
+  nvim $1
+  echo "is a file"
+  exit 0
+fi
--- scripts/cdFuzz.sh
+++ scripts/cdFuzz.sh
@@ -13,21 +13,25 @@ while getopts 't:' flag; do # check flag status
   esac
 done
 
+echo "$t_flag"
 
-if  [[ "$t_flag" == "" ]]; then
-  echo "$t_flag"
-  choice=$( fd . "$dir" --type d --exclude node_modules | fzf )
+if [[ "$t_flag" != "" ]]; then #if t flag is empty that means to things one that it could be not chosen or two that the user did use -t but did not enter a dir name
+  choice=$( fd . ./"$t_flag" --type d --exclude go -E node_modules | fzf ) && cd $choice
   sessionName=$(basename $PWD)
-  tmux new-session -A -d -s "$sessionName" -c "$choice" # makes tmux session call dev 
-  # tmux send-keys -t "$sessionName" "cd "$choice"" C-m # runs command in tmux session windows end C-m enters it 
+  tmux new-session -A -d -s "$sessionName" # makes tmux session call dev 
+  # tmux send-keys -t "$sessionName" ""$EDITOR" \"$file\"" C-m # runs command in tmux session windows end C-m enters it 
   tmux attach -t "$sessionName" # now we attach into the made tmux session
   return 0
 fi
 
-echo "$dir"
-if [[ "$dir" == "" ]]; then # this means current dir by default
-  choice=$(fd . ./ -t d --exclude go | fzf) && cd $choice 
-  return 1
+dir="$1"
+if [[ "$dir" == "" ]]; then
+  choice=$( fd . ./ --type d --exclude go -E node_modules | fzf ) && cd $choice
+  return 0
+else
+  choice=$( fd . ./$dir/ --type d --exclude go -E node_modules | fzf ) && cd $choice
+  return 0
 fi
-choice=$(fd . ./$dir/ -t d --exclude node_modules | fzf) && cd $choice
-return 0
+  
+# choice=$( fd . ./$dir/ --type d --exclude go -E node_modules | fzf ) && cd $choice
+# return 0
