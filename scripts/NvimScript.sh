21e7af83eb6fb4b820f003ee0d50540d85fa4171
Boofny <Brandonpizano7557@gmail.com>
2026-01-20 14:20:21 -0500

    updating vim scripts

--- scripts/NvimScript.sh
+++ scripts/NvimScript.sh
@@ -8,18 +8,18 @@
 
 if [[ "$1" == "" ]]; then  # just the neovim command alone open the current dir
   choice=$( fd . ./ --type file --exclude node_modules -E go -E .git | fzf --preview 'bat --style=numbers --color=always --line-range :500 {}') || exit 0
-  nvim $choice
+  vim $choice
   exit 0
 fi
 
 if [[ -d "$1" ]]; then # this checks if a input is a file or dir
   choice=$( fd . ./"$1" --type file --exclude node_modules -E go -E .git | fzf --preview 'bat --style=numbers --color=always --line-range :500 {}') || exit 0
   echo $choice 
-  nvim $choice
+  vim $choice
   echo "is a dir"
   exit 0
 else 
-  nvim $1
+  vim $1
   echo "is a file"
   exit 0
 fi
