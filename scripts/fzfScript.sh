6afcf434d8a7d841eaa6c556a7a1a583de906b59
Boofny <Brandonpizano7557@gmail.com>
2026-01-23 17:45:26 -0500

    some better of life updates

---
+++ scripts/fzfScript.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#this is just a script to add a preveow flag to fzf 
+#this script will be sourced
+
+preview_flag=false
+
+while getopts 'p' flag; do # check flag status
+  case "${flag}" in
+    p) preview_flag=true;;
+    *) return 1
+  esac
+done
+
+if $preview_flag ; then
+  fzf --style full --preview 'bat --color=always --style=numbers --line-range=:500 {}' --margin=10%,10% --layout reverse --border --color 'border:#89b5fa' --border-label 'FZF' || exit 1
+  exit 0
+fi
+
+fzf --style full --margin=10%,10% --layout reverse --border --color 'border:#89b5fa' --border-label 'FZF' || exit 0
--- scripts/vimer.sh
+++ scripts/vimer.sh
@@ -14,12 +14,9 @@ if [[ -d "$1" ]]; then # this checks if a input is a file or dir
   fzf --style full --preview 'bat --color=always --style=numbers --line-range=:500 {}' --margin=10%,10% --layout reverse --border --color 'border:#89b5fa' --border-label 'Vim') ||
   exit 0
   vim $choice
-
-  echo "is a dir"
   exit 0
 else 
   vim $1
-  echo "is a file"
   exit 0
 fi
 
