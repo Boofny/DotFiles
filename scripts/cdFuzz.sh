cb124aca182c5c051730bfd9a9314c2160f44e18
Boofny <Brandonpizano7557@gmail.com>
2026-01-31 13:06:21 -0500

    did not like how the tmux one worked will rework in the future

--- scripts/cdFuzz.sh
+++ scripts/cdFuzz.sh
@@ -2,26 +2,26 @@
 
 #source this script
 
-t_flag=false # tmux flag
+# t_flag=false # tmux flag
+#
+# while getopts 't' flag; do # check flag status
+#   case "${flag}" in
+#     t) t_flag=true;;
+#     *) return 1
+#   esac
+# done
 
-while getopts 't' flag; do # check flag status
-  case "${flag}" in
-    t) t_flag=true;;
-    *) return 1
-  esac
-done
+# echo "$t_flag"
 
-echo "$t_flag"
-
-if $t_flag; then #if t flag is empty that means to things one that it could be not chosen or two that the user did use -t but did not enter a dir name
-  choice=$( fd . ./ --type d --exclude go -E node_modules | 
-  \fzf --style full --margin=10%,10% --layout reverse --border --color 'border:#89b5fa' --border-label 'CDF'
-  ) && cd $choice || return 1
-  sessionName=$(basename $PWD)
-  tmux new-session -A -d -s "$sessionName" # makes tmux session call dev 
-  tmux attach -t "$sessionName" # now we attach into the made tmux session
-  return 0
-fi
+# if $t_flag; then #if t flag is empty that means to things one that it could be not chosen or two that the user did use -t but did not enter a dir name
+#   choice=$( fd . ./ --type d --exclude go -E node_modules | 
+#   \fzf --style full --margin=10%,10% --layout reverse --border --color 'border:#89b5fa' --border-label 'CDF'
+#   ) && cd $choice || return 1
+#   sessionName=$(basename $PWD)
+#   tmux new-session -A -d -s "$sessionName" # makes tmux session call dev 
+#   tmux attach -t "$sessionName" # now we attach into the made tmux session
+#   return 0
+# fi
 
 # fzf --style full --margin=10%,10% --layout reverse --border --color 'border:#89b5fa' --border-label 'Vim'
 dir="$1"
