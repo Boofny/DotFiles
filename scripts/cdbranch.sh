e572d49424774ca16588b0860a95c03f791949a1
Boofny <Brandonpizano7557@gmail.com>
2026-05-25 15:47:14 -0400

    made the fzf inline for cb

--- scripts/cdbranch.sh
+++ scripts/cdbranch.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
 # works for now gonna test it out to see how it works 
-branch=$(git branch | fzf --style full --margin=20%,20% --layout reverse --border --color 'border:#89b5fa' --border-label 'Branches')
+# branch=$(git branch | fzf --style full --margin=20%,20% --layout reverse --border --color 'border:#89b5fa' --border-label 'Branches')
+branch=$( git branch | fzf --height 40% --layout=reverse --info=inline --no-separator )
 
 git checkout $branch
