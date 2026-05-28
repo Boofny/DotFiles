12e52709da11c80af391c330385863b5f45f2154
Boofny <Brandonpizano7557@gmail.com>
2026-05-28 17:54:58 -0400

    nvm

--- scripts/cdFuzz.sh
+++ scripts/cdFuzz.sh
@@ -9,40 +9,38 @@ if [[ "$dir" == "" ]]; then
   choice=$( fd . ./ --type d --exclude go -E node_modules | 
   \fzf --style full --margin=0%,0% --layout reverse --border --color 'border:#89b5fa' --border-label 'CDF'
   ) && cd $choice || return 1
-  git status
 
-  # if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-  #   printf "\033[33m Git Pull? [y]: \033[0m"
-  #   read pull
-  #   if [[ "$pull" == "y" ]]; then 
-  #     printf "\033[32m Pulling...\033[0m"
-  #     git pull
-  #     return 0
-  #   fi
-  #
-  # else
-  #   return 1
-  # fi
+  if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    printf "\033[33m Git Pull? [y]: \033[0m"
+    read pull
+    if [[ "$pull" == "y" ]]; then 
+      printf "\033[32m Pulling...\033[0m"
+      git pull
+      return 0
+    fi
+
+  else
+    return 1
+  fi
 
 else
   choice=$( fd . "$1" --type d --exclude go -E node_modules | 
   \fzf --style full --margin=0%,0% --layout reverse --border --color 'border:#89b5fa' --border-label 'CDF'
   ) && cd $choice || return 1
 
-  git status
-  # if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-  #
-  #   printf "\033[33m Git Pull? [y]: \033[0m"
-  #   read pull
-  #   if [[ "$pull" == "y" ]]; then 
-  #     printf "\033[31m Pulling...\033[0m"
-  #     git pull
-  #     return 0
-  #   fi
-  #
-  # else
-  #   return 1
-  # fi
+  if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+
+    printf "\033[33m Git Pull? [y]: \033[0m"
+    read pull
+    if [[ "$pull" == "y" ]]; then 
+      printf "\033[31m Pulling...\033[0m"
+      git pull
+      return 0
+    fi
+
+  else
+    return 1
+  fi
 
 fi
   
