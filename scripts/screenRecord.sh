aa155f0739e0a8a150c046f55d13c8ee5ebcde32
Boofny <Brandonpizano7557@gmail.com>
2026-08-14 10:01:32 -0400

    update screen record

--- scripts/screenRecord.sh
+++ scripts/screenRecord.sh
@@ -8,5 +8,5 @@ fi
 
 file_name="$1.mp4"
 
-ffmpeg -video_size 1920x1080 -framerate 25 -f x11grab -i :0.0 ~/Videos/$file_name
+ffmpeg -video_size 1920x1080 -framerate 25 -f x11grab -i :0.0 "$HOME/videos/$file_name"
 
