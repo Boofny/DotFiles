7830f0d8ea5a13d886d4c4d483094e7cb5f0db8e
Boofny <Brandonpizano7557@gmail.com>
2026-08-16 14:20:51 -0400

    adding custom flag for frame rate on screen record

--- scripts/screenRecord.sh
+++ scripts/screenRecord.sh
@@ -1,5 +1,14 @@
 #! /usr/bin/env bash
 
+# Parse -f flag for frame rate
+while getopts "f:" opt; do
+  case $opt in
+    f) framerate="$OPTARG" ;;
+    *) echo "Usage: $0 -f <framerate>"; exit 1 ;;
+  esac
+done
+
+framerate="${framerate:-30}"
 
 if [[ "$1" == "" ]]; then
     printf "\033[33mNeed a file name!\033[0m"
@@ -8,5 +17,5 @@ fi
 
 file_name="$1.mp4"
 
-ffmpeg -video_size 1920x1080 -framerate 25 -f x11grab -i :0.0 "$HOME/videos/$file_name"
+ffmpeg -video_size 1920x1080 -framerate "$framerate" -f x11grab -i :0.0 "$HOME/videos/$file_name"
 
